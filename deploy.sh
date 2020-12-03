8a95d21455aefc9beaf2ec6ae21ececee97ce8d1
RomanMeleshko <roman.meleshko1@gmail.com>
2020-12-03 17:33:27 +0200

    Second

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:PornoFalos/PornoFalos.github.io master:gh-pages
+# git push -f git@github.com:PornoFalos/PornoFalos.github.io master:gh-pages
 
 cd -
