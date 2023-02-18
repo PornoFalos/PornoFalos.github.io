56fb5e439b2555c90eba30a1b3d1978eb7dcbcb1
Roman <roman@macbook.local>
2023-02-19 00:48:25 +0200

    fix/title-logo Added logo header

--- src/components/Header/HeaderNavBar.vue
+++ src/components/Header/HeaderNavBar.vue
@@ -5,7 +5,7 @@
         <nav class="navbar navbar-expand-lg navbar-light p-4">
           <a class="navbar-brand" href="#">
             <img src="@/assets/logoCRM.gif" width="30" height="30" class="d-inline-block" alt="">
-              <span class="ml-3 text-logo">FineCRM</span><span class="ml-2">doc</span>
+              <span class="ml-3 text-logo">Ebalkin</span>
           </a>
           <div v-on:click="$emit('show-menu', show)" class="navbar-button text-center p-2">
             <i class="fas fa-align-justify"></i>
